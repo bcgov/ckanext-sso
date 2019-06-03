fa73aaa42221556ddb24afcfb38ea2c4c3cfe937
Brandon Sharratt <brandon@highwaythreesolutions.com>
2019-06-03 16:16:14 -0700

    added group support

--- ckanext/sso/helper.py
+++ ckanext/sso/helper.py
@@ -23,10 +23,12 @@ class SSOHelper(object):
         self.profile_email_field = config.get('ckan.sso.profile_email_field', None)
         self.profile_group_field = config.get('ckan.sso.profile_group_field', None)
         self.sysadmin_group_name = config.get('ckan.sso.sysadmin_group_name', None)
+        self.profile_group_delim = config.get('ckan.sso.profile_group_delim', None)
         realm = KeycloakRealm(server_url=self.authorization_endpoint, realm_name=self.realm)
         self.oidc_client = realm.open_id_connect(client_id=self.client_id,client_secret=self.client_secret)
 
     def identify(self, token):
+        log.info("SSO Identify")
         user_token = self.oidc_client.userinfo(token)
         # user_data = self.oidc_client.decode_token(user_token, '', options={ 'verify_signature': False })
         import jwt
@@ -38,9 +40,6 @@ class SSOHelper(object):
         except :
             log.debug("Not Found User Name.")
 
-        user = model.User.get(user_name)
-        if user :
-            return user.name
         user = None
         users = model.User.by_email(email)
         if len(users) == 1:
@@ -55,8 +54,57 @@ class SSOHelper(object):
                 user.sysadmin = True
             else:
                 user.sysadmin = False
+
+        log.info('Add user into ckan database: %s'%user)
         model.Session.add(user)
         model.Session.commit()
         model.Session.remove()
-        log.info('Add keycloak user into ckan database: %s'%user)
+
+        changedGroups = False
+        if self.profile_group_field and self.profile_group_delim and self.profile_group_field in user_data:
+            membership = model.Session.query(model.Member).filter(model.Member.table_name == 'user').filter(model.Member.table_id == user.id).all()
+            
+            for group in user_data[self.profile_group_field]:
+
+                group = group.lstrip(self.profile_group_delim)
+                
+                group = group.split(self.profile_group_delim)
+
+                if len(group) > 1:
+                    group_name = "".join(group[0].strip())
+                    capacity = group[1].lower()
+
+                    dbGroup = model.Session.query(model.Group).filter(model.Group.name == group_name).first()
+                    if not dbGroup is None:
+                        
+                        capacity = capacity.lower()
+
+                        if capacity in ["admin", "editor", "member"]:
+                            memberDb = None
+                            for memberOf in membership:
+                                if memberOf.group_id == dbGroup.id and memberOf.capacity == capacity and memberOf.state == 'active':
+                                    memberDb = memberOf
+                                    break
+
+                            if not memberDb is None:
+                                membership.remove(memberDb)
+
+                            if memberDb is None:
+                                member = model.Member(table_name='user', table_id=user.id, capacity=capacity, group=dbGroup)
+                                log.info('Add user %s into group %s', user.name, dbGroup.name)
+                                rev = model.repo.new_revision()
+                                rev.author = user.id
+                                model.Session.add(member)
+                                changedGroups = True
+
+            for memberRec in membership:
+                changedGroups = True
+                log.info('Removing user %s from group %s', user.name, memberRec.group_id)
+                model.Session.delete(memberRec)
+
+
+            if changedGroups:
+                model.Session.commit()
+                model.Session.remove()
+
         return user.name
