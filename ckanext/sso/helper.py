f309bc05a0ed69bfc96a39ce7be7d9a278868c50
John Churchill <john.churchill@gov.bc.ca>
2023-11-03 14:14:06 -0700

    Added more logging

--- ckanext/sso/helper.py
+++ ckanext/sso/helper.py
@@ -58,8 +58,9 @@ class SSOHelper(object):
                 self.profile_group_field] or self.profile_group_delim + self.sysadmin_group_name in user_data[
                                                  self.profile_group_field]):
                 user.sysadmin = True
-
-        log.info('Add user into ckan database: %s'%user_data)
+        log.info('profile group field: %s'%self.profile_group_field)
+        log.info('sysadmin group name: %s'%self.profile_group_field)
+        log.info('Add user into ckan database: %s'%user_data[self.profile_group_field])
         model.Session.add(user)
         model.Session.commit()
         model.Session.remove()
