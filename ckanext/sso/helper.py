dbdb23f1cd8502bedd40f40d42e4bbd98beb9767
John Churchill <john.churchill@gov.bc.ca>
2025-03-25 15:24:35 -0700

    Edited logging

--- ckanext/sso/helper.py
+++ ckanext/sso/helper.py
@@ -82,7 +82,7 @@ class SSOHelper(object):
 
         group_added = False
         for group in groups_to_join:
-            log.info('Add user into group : %s'%group.group_id)
+            log.info('Add user into group : .%s.'%group.group_id)
             # group_d = group._asdict()
             member = model.Member(table_name='user', table_id=user.id, capacity='member', group_id=group.group_id)
             model.Session.add(member)
